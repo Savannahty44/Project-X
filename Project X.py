5849f220a92d676ad272675e893b9bc06929d5fd
Savannahty44 <41313341+Savannahty44@users.noreply.github.com>
2018-07-16 23:24:46 -0300

    Add files via upload

---
+++ Project X.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Savannah T
+"""
+
+import os 
+import pandas as pd
+
+
+for root, directory, files in os.walk(r'C:\Users\Savannah\Google Drive\Savannah\Project X'):
+    for file in files:
+        filename = os.path.join(root, file)
+        #df = pd.read_excel(filename)
+        #value = df['E/S'][]
+        #xls = pd.ExcelFile(filename)
+        df_lancamentos = pd.read_excel(filename, sheetname = 0, skiprows= 3)
+        print(df_lancamentos)
+        
+
+    
+        
